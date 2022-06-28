cbe94a11ab651554741cfde69faa663f25808110
Bohdan-Ta <81333397+Bohdan-Ta@users.noreply.github.com>
2022-06-29 01:13:49 +0300

    fix weather

--- src/components/Weather/index.jsx
+++ src/components/Weather/index.jsx
@@ -1,6 +1,6 @@
-// import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import getWeather from '../../services/getWeather'
 
 const Wethers = styled.div`
 	position: absolute;
@@ -17,18 +17,18 @@ const Wethers = styled.div`
 `
 
 export default function Weather() {
-	async function getWeather() {
-		try {
-			const { data } = await axios.get(
-				'https://api.openweathermap.org/data/2.5/weather?lat=50.463807&lon=30.569740&APPID=e4bdf24f044b57ecdb9bbc9845a5e899'
-			)
-			console.log(data)
-			return data
-		} catch (error) {
-			console.error(error)
+	const [weather, setWeather] = useState([])
+
+	useEffect(() => {
+		const getTemp = async () => {
+			const result = await getWeather()
+			console.log(result)
+			setWeather(result)
 		}
-	}
-	const { wind } = getWeather()
-	console.log(wind)
-	return <Wethers>{wind}</Wethers>
+
+		getTemp()
+	}, [])
+
+	const { name } = weather
+	return <Wethers>{name}</Wethers>
 }
---
+++ src/services/getWeather.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const weather = axios.create({
+	baseURL: 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=e4bdf24f044b57ecdb9bbc9845a5e899',
+	timeout: 1000,
+})
+
+export default async function getWeather() {
+	try {
+		const response = await weather()
+		return response.data
+	} catch (error) {
+		console.error(error)
+	}
+}
+
+getWeather()
